5d020a5aed4de336e5bac872b4e84b157699fd5b
yaredghebre <yared2391@gmail.com>
2023-12-12 18:00:47 +0100

    added Logout

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,25 +1,42 @@
-import { Link } from "react-router-dom";
+import React from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { useAuth } from '../contexts/AuthProvider';
+
+const NavBarLink = ({ href, onClick, children }) => {
+  return (
+    <Link
+      to={href}
+      onClick={onClick}
+      className="transition-all duration-300 hover:bg-gray-100 hover:text-red-500"
+    >
+      {children}
+    </Link>
+  );
+};
 
 const Header = () => {
+  const { handleLogOut, isLogged } = useAuth();
+  const navigate = useNavigate();
+
   return (
     <div>
-      <nav class="bg-white border-gray-200 dark:bg-gray-900">
-        <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+      <nav class="border-gray-200 bg-white dark:bg-gray-900">
+        <div class="mx-auto flex max-w-screen-xl flex-wrap items-center justify-between p-4">
           <Link to="/" class="flex items-center space-x-3 rtl:space-x-reverse">
-            <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white ">
+            <span class="self-center whitespace-nowrap text-2xl font-semibold dark:text-white ">
               My Blog.
             </span>
           </Link>
           <button
             data-collapse-toggle="navbar-dropdown"
             type="button"
-            class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+            class="inline-flex h-10 w-10 items-center justify-center rounded-lg p-2 text-sm text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600 md:hidden"
             aria-controls="navbar-dropdown"
             aria-expanded="false"
           >
             <span class="sr-only">Open main menu</span>
             <svg
-              class="w-5 h-5"
+              class="h-5 w-5"
               aria-hidden="true"
               xmlns="http://www.w3.org/2000/svg"
               fill="none"
@@ -35,11 +52,17 @@ const Header = () => {
             </svg>
           </button>
           <div class="hidden w-full md:block md:w-auto" id="navbar-dropdown">
-            <ul class="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+            <ul class="mt-4 flex flex-col rounded-lg border border-gray-100 bg-gray-50 p-4 font-medium rtl:space-x-reverse dark:border-gray-700 dark:bg-gray-800 md:mt-0 md:flex-row md:space-x-8 md:border-0 md:bg-white md:p-0 md:dark:bg-gray-900">
+              {isLogged ? (
+                <li>
+                  <NavBarLink onClick={handleLogOut}>Logout</NavBarLink>
+                </li>
+              ) : null}
+
               <li>
                 <Link
                   to="/"
-                  class="block py-2 px-3 text-gray-900 bg-blue-700 rounded md:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500"
+                  class="block rounded bg-blue-700 px-3 py-2 text-gray-900 dark:text-white md:bg-transparent md:p-0 md:hover:text-blue-700 md:dark:text-blue-500"
                   aria-current="page"
                 >
                   Home
@@ -50,12 +73,12 @@ const Header = () => {
                   id="dropdownHoverButton"
                   data-dropdown-toggle="dropdownHover"
                   data-dropdown-trigger="hover"
-                  class="flex items-center justify-between w-full py-2 px-3 text-gray-900 rounded transition transform duration-150 hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 md:w-auto dark:text-white md:dark:hover:text-blue-700 dark:focus:text-white dark:border-gray-700 dark:hover:bg-gray-700 md:dark:hover:bg-transparent"
+                  class="flex w-full transform items-center justify-between rounded px-3 py-2 text-gray-900 transition duration-150 hover:bg-gray-100 dark:border-gray-700 dark:text-white dark:hover:bg-gray-700 dark:focus:text-white md:w-auto md:border-0 md:p-0 md:hover:bg-transparent md:hover:text-blue-700 md:dark:hover:bg-transparent md:dark:hover:text-blue-700"
                   type="button"
                 >
                   Posts
                   <svg
-                    class="w-2.5 h-2.5 ms-2.5"
+                    class="ms-2.5 h-2.5 w-2.5"
                     aria-hidden="true"
                     xmlns="http://www.w3.org/2000/svg"
                     fill="none"
@@ -74,7 +97,7 @@ const Header = () => {
                 {/* Dropdown Menu */}
                 <div
                   id="dropdownHover"
-                  class="z-10 hidden font-normal bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700 dark:divide-gray-600"
+                  class="z-10 hidden w-44 divide-y divide-gray-100 rounded-lg bg-white font-normal shadow dark:divide-gray-600 dark:bg-gray-700"
                 >
                   <ul
                     class="py-2 text-sm text-gray-700 dark:text-gray-400"
@@ -102,7 +125,7 @@ const Header = () => {
               <li>
                 <Link
                   to="/about"
-                  class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+                  class="block rounded px-3 py-2 text-gray-900 hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:border-0 md:p-0 md:hover:bg-transparent md:hover:text-blue-700 md:dark:hover:bg-transparent md:dark:hover:text-blue-500"
                 >
                   About
                 </Link>
